0355eb3aa6c49d233880440b4a695f842cc7d9fb
simon <simon.dupkala@gmail.com>
2022-10-28 14:14:58 +0200

    fixed main img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,38 +15,21 @@ const Home = () => {
   }, []);
   return (
     <div className={` ${classes.main}`}>
-      {/* <div className={classes.imageBox}>
-         <div className={classes.image}></div>
-      </div> */}
       <div style={{ width: "90vw", margin: "auto" }}>
-        <div
-          style={{
-            maxWidth: "1260px",
-            /* marginTop: "30px", */
-            paddingBottom: "20px",
-            position: "relative",
-          }}
-        >
-          <img src={HomeImg} style={{ width: "80vw", borderRadius: "20px" }} />
-
-          <div
-            className="position-absolute"
-            style={{
-              position: "absolute",
-              top: "50px",
-              left: "20px",
-            }}
-          >
-            <h2 className="text-white" style={{ fontWeight: "bold" }}>
-              Kryje vám záda doma i venku
-            </h2>
-            <p
-              className="text-white"
-              style={{ width: "80%", paddingBottom: "20px" }}
-            >
-              PEVNÝ I MOBILNÍ INTERNET S NEJVĚTŠÍM POKRYTÍM
-            </p>
-            <button className="basicBtn">Chci ušetřit</button>
+        <div>
+          <div className={classes.secondImg}>
+            <div className={classes.innerImg}>
+              <h2 className="text-white" style={{ fontWeight: "bold" }}>
+                Kryje vám záda doma i venku
+              </h2>
+              <p
+                className="text-white"
+                style={{ width: "80%", paddingBottom: "20px" }}
+              >
+                PEVNÝ I MOBILNÍ INTERNET S NEJVĚTŠÍM POKRYTÍM
+              </p>
+              <button className="basicBtn">Chci ušetřit</button>
+            </div>
           </div>
         </div>
       </div>
